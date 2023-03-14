836981ec684047366b16268c300c6c59c1ea890d
diegoG82 <diegogiardina@hotmail.it>
2023-03-14 16:54:15 +0100

    Inserita immagine statica

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// **Consegna:**
+    // **Consegna:**
 // Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
 // **MILESTONE 1**
 // Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
@@ -8,6 +8,37 @@
 // Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
 // **MILESTONE 3**
 // Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
+
+// Input
+// Creo Array delle immagini
+
+const images = [
+  "img/01.jpg",
+  "img/02.jpg",
+  "img/03.jpg",
+  "img/04.jpg",
+  "img/05.jpg",
+];
+
+// inserisco immagini 
+
+const item = document.querySelector('.slider-items');
+
+for (let i = 0; i < images.length; i++){
+    const currentImg = images[i];
+    const sliderItems =  `<div class="item"><img src="${currentImg}"></div>`;
+    item.innerHTML += sliderItems;
+}
+
+const itemArray = document.getElementsByClassName("item");
+
+let firstImage = 0;
+
+itemArray[firstImage].classList.add("active");
+
+
+
+
 
 
 
